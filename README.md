97281ad29730ce9d83ae0418237e9583395aae14
Jeong Han Lee <jeonghan.lee@gmail.com>
2021-06-05 23:32:49 -0700

    add gitlab ci for CentOS7

--- README.md
+++ README.md
@@ -7,7 +7,20 @@
 This script is developed to reduce the workflow that is defined in EPICS IOC Development Guide (AL-1451-7926).
 The script must be called in a directory where the script is located.
 
-'''bash
+```bash
 bash tools/generate_ioc_structure.bash -l test -a tctemp
+```
+
+In case, one wants to add the gitlab CI configuration into the existing folder.
+
+```bash
+cd tctemp
+bash ../tools/generate_ioc_structure.bash -c -a
+```
+
+In case, one wants to create everything together.
+
+```bash
+bash tools/generate_ioc_structure.bash -l test -a tctemp -c
 ```
 
--- generate_ioc_structure.bash
+++ generate_ioc_structure.bash
@@ -17,8 +17,8 @@
 #
 # Author  : Jeong Han Lee
 # email   : JeongLee@lbl.gov
-# Date    : Fri Jun  4 20:16:05 PDT 2021
-# version : 0.0.2
+# Date    : Sat Jun  5 23:14:23 PDT 2021
+# version : 0.0.3
 
 declare -g SC_RPATH;
 #declare -g SC_NAME;
@@ -35,10 +35,12 @@ function usage
 {
     {
 	echo "";
-	echo "Usage    : $0 [-n APPNAME] [-l LOCATION]"
+	echo "Usage    : $0 [-n APPNAME] [-l LOCATION] <-c> <-a>"
 	echo "";
 	echo "               -n : APPNAME"
 	echo "               -l : LOCATION"
+    echo "               -c : Optional : Add git, and gitlab ci"
+    echo "               -a : Optional : WITHIN an existing a , add git, and gitlab ci"
 	echo "";
 	echo " bash $0 -n APPNAME -l LOCATION"
 	echo ""
@@ -47,27 +49,186 @@ function usage
     exit 1;
 }
 
+# Must call within git repo path
+function add_gitignore
+{
+    cat > .gitignore <<EOF
+# EPICS site : https://epics-controls.org/
+# References : epics-base / epics-modules / @ralphlange / @jeonghanlee
+# Directories built by EPICS building system 
+/cfg/
+/bin/
+/lib/
+/dbd/
+/db/
+/html/
+/include/
+/templates/
+O.*/
+/*Top/cfg/
+/*Top/bin/
+/*Top/lib/
+/*Top/db/
+/*Top/dbd/
+/*Top/html/
+/*Top/include/
+/*Top/templates/
 
-options="n:l:"
+
+# User-specific files for local modifications
+/configure/*.local
+/configure/RELEASE.*
+/configure/CONFIG_SITE.*
+/modules/RELEASE.*.local
+/modules/Makefile.local
+/*Top/configure/*.local
+
+# documents
+/documentation/html
+/documentation/*.tag
+
+# Others for UI, autosave, and so on
+/QtC-*
+envPaths
+cdCommands
+dllPath.bat
+*BAK.adl
+auto_settings.sav*
+auto_positions.sav*
+
+*~
+.\#*
+\#*
+.versions
+*-src
+Payara-src
+*-amazon-corretto-*_amd64.deb*
+*.service
+*.list
+*.swp
+EOF
+}
+
+function add_gitattributes
+{
+   cat > .gitattributes <<EOF
+# Set the default behavior, in case people don't have core.autocrlf set.
+* text=auto
+
+# Explicitly declare text files you want to always be normalized and converted
+# to native line endings on checkout.
+*.c text
+*.h text
+
+# Declare files that will always have CRLF line endings on checkout.
+*.sln text eol=crlf
+
+# Denote all files that are truly binary and should not be modified.
+*.png binary
+*.jpg binary
+EOF
+}
+
+
+# Must call it within git repo path
+function add_submodule
+{
+    local src_url="$1"; shift;
+    local tgt_name="$1"; shift;
+    printf "${src_url} is adding as submodule ${tgt_name}.\n";
+    git submodule add ${src_url} ${tgt_name}  ||  die 1 "We cannot add ${src_url} as submodule : Please check it" ;
+    printf "\n";
+    git submodule update --init --recursive  ||  die 1 "We cannot init the gitsubmodule : Please check it" ;
+}  
+
+function epics_ci
+{
+    local url="https://github.com/epics-base/ci-scripts";
+    local tgt=".ci";
+    add_submodule "$url" "$tgt";
+    mkdir -p .ci-local
+    echo "BASE=7.0" > .ci-local/stable.set
+    git add .ci-local/stable.set
+    cat > .gitlab-ci.yml <<EOF
+# .gitlab-ci.yml for testing EPICS Base ci-scripts
+# (see: https://github.com/epics-base/ci-scripts)
+
+cache:
+  key: "$CI_JOB_NAME-$CI_COMMIT_REF_SLUG"
+  paths:
+    - .cache/
+
+variables:
+  GIT_SUBMODULE_STRATEGY: "recursive"
+  SETUP_PATH: ".ci-local:.ci"
+  BASE_RECURSIVE: "NO"
+  CMP: "gcc"
+  BGFC: "default"
+
+# Template for build jobs (hidden)
+.build:
+  image: centos:7.9.2009
+  stage: build
+  before_script:
+    - cat /etc/os-release
+    - yum -y install git bash sudo
+    - yum -y update
+    - git clone https://github.com/jeonghanlee/pkg_automation
+    - bash ${CI_PROJECT_DIR}/pkg_automation/pkg_automation.bash -y
+    - python .ci/cue.py prepare
+  script:
+    - python .ci/cue.py build
+    - python .ci/cue.py test
+    - python .ci/cue.py test-results
+
+# Build on Linux using default gcc for Base branches 7.0 and 3.15
+
+gcc_base_7_0:
+  extends: .build
+  variables:
+    BASE: "7.0"
+    SET: stable
+
+gcc_base_3_15:
+  extends: .build
+  variables:
+    BASE: "3.15"
+    SET: stable
+
+ShellCheck:
+    image: alpine
+    stage: test
+    before_script:
+    - apk update
+    - apk --no-cache add bash git shellcheck
+    script:
+    - git ls-files --exclude='*.bash' --ignored | xargs shellcheck
+EOF
+}
+
+options="n:l:cia"
 APPNAME=""
 LOCATION=""
+EPICS_CI="NO"
 APPNAME_EXIST="FALSE"
+ADDONLYCONFIG="NO"
 
 while getopts "${options}" opt; do
     case "${opt}" in
         n) APPNAME=${OPTARG}   ;;
-	l) LOCATION=${OPTARG}  ;;
-   	:)
-	    echo "Option -$OPTARG requires an argument." >&2
-	    usage
+	    l) LOCATION=${OPTARG}  ;;
+        c) EPICS_CI="YES"      ;;
+        a) ADDONLYCONFIG="YES" ;;
+   	    :)
+	        echo "Option -$OPTARG requires an argument." >&2
+	        usage
 	    ;;
-	h)
-	    usage
+	    h)
+	        usage
 	    ;;
-	\?)
+        \?)
 	    echo "Invalid option: -$OPTARG" >&2
 	    usage
-            CATION
 	    ;;
     esac
 done
@@ -86,59 +247,65 @@ if [ -z "$EPICS_BASE" ]; then
     exit;
 fi
 
-if [ -z "$APPNAME" ]; then
-    usage;
-fi
+if [[ "$ADDONLYCONFIG" == "NO" ]]; then
 
-if [ -z "$LOCATION" ]; then
-    usage;
-fi
-
-TOP=${PWD};
-
-if [[ "${TOP}" == "$SC_TOP" ]]; then
-    echo "Please call $0 outside ${SC_TOP}"
-    exit;
-fi
-
-
-APPTOP="${TOP}/${APPNAME}"
-
-mkdir -p "${APPTOP}"
-pushd "${APPTOP}" || exit
-
-for folder in *
-do
-    if test "${folder#*$APPNAME}" != "$folder"; then
-        APPNAME_EXIST="TRUE";
+    if [ -z "$APPNAME" ]; then
+        usage;
     fi
-done
 
-if [[ "$APPNAME_EXIST" == "FALSE" ]]; then
-    makeBaseApp.pl -t ioc "${APPNAME}"
+    if [ -z "$LOCATION" ]; then
+        usage;
+    fi
+
+    TOP=${PWD};
+    if [[ "${TOP}" == "$SC_TOP" ]]; then
+        echo "Please call $0 outside ${SC_TOP}"
+        exit;
+    fi
+
+    APPTOP="${TOP}/${APPNAME}"
+
+    mkdir -p "${APPTOP}"
+    pushd "${APPTOP}" || exit
+
+    for folder in *
+        do
+        if test "${folder#*$APPNAME}" != "$folder"; then
+            APPNAME_EXIST="TRUE";
+        fi
+    done
+
+    if [[ "$APPNAME_EXIST" == "FALSE" ]]; then
+        makeBaseApp.pl -t ioc "${APPNAME}"
+    fi
+
+    IOCNAME="${LOCATION}-${APPNAME}"
+    makeBaseApp.pl -i -t ioc -p "${APPNAME}" "${IOCNAME}"
+
+    README=README.md
+
+    if [[ ! -f "${README}" ]]; then
+        echo "# EPICS IOC for ${IOCNAME}"  > "${README}"
+        echo ""                           >> "${README}"
+        echo ""                           >> "${README}"
+    fi
 fi
 
-IOCNAME="${LOCATION}-${APPNAME}"
-makeBaseApp.pl -i -t ioc -p "${APPNAME}" "${IOCNAME}"
-
-
-README=README.md
-
-if [[ ! -f "${README}" ]]; then
-    echo "# EPICS IOC for ${IOCNAME}"  > "${README}"
-    echo ""                           >> "${README}"
-    echo ""                           >> "${README}"
+if [[ "$EPICS_CI" == "YES" ]]; then
+   if [ ! -d .git ]; then
+    git init;
+   fi
+   epics_ci;
+   add_gitignore;
+   add_gitattributes;
+   git add . -u;
+   git add --renormalize .
+else
+    echo "--------"
+    echo "  Please create ${APPNAME} as Project Name in the ALS git server.";
+    echo "  ${APPNAME} also is used for Project slug in the gitlab server.";
+    echo "  ";
+    echo "  After this, one may need to execute the following command:";
+    echo "  git remote add origin ssh:........./${APPNAME}.git"
+    echo "--------"
 fi
-
-
-# git init
-# git add .
-
-echo "--------"
-echo "  Please create ${APPNAME} as Project Name in the ALS git server.";
-echo "  ${APPNAME} also is used for Project slug in the gitlab server.";
-echo "  ";
-echo "  After this, one may need to execute the following command:";
-echo "  git remote add origin ssh:........./${APPNAME}.git"
-echo "--------"
-
